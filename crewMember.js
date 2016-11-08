521a5249d757399787963fac2476c3b522fd01fa
github username <github email address>
2016-11-08 16:06:29 -0500

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,54 +1,38 @@
+class CrewMember {
+ constructor(object) {
+   this.id = object.id;
+   this.position = object.position
+   this.currentShip = 'Looking for a Rig'
+   Store.data['crewMembers'].push(this)
+ }
 
+ assignShip(spaceship) {
+     this.currentShip = spaceship
+     this.currentShip.docked = false
+     this.spaceshipId = spaceship.id
+ }
 
+ chargePhasers() {
+   if (this.position === 'Gunner' && this.spaceshipId) {
+     this.currentShip.phasersCharge = 'charged!'
+   } else {
+     return 'had no effect'
+   }
+ }
 
- const CrewMember = (function createCrewMember(){
-  // var id = 0;
-return class {
-  constructor(data, ship_id){
-    this.id = data.id;
-    this.position = data.position;
-    this.ship_id = ship_id
-    Store.data.crewMembers.push(Object.assign({}, {crewMembers: [...Store.data.crewMembers, this]}))
-  }
-  spaceship(){
-      return Store.data.spaceships.filter((spaceship) => {
-        return spaceship.id === this.ship_id;
-      })}
+ engageWarpDrive() {
+   if (this.position === 'Pilot' && this.spaceshipId) {
+     this.currentShip.warpDrive = 'engaged!'
+   } else {
+     return 'had no effect'
+   }
+ }
 
-      // engageWarpDrive(){
-      //   if
-      //
-      // };
-      // setsInvisibility(){
-      //
-      // };
-      // chargePhasers(){
-      //
-      // };
+ setsInvisibility() {
+   if (this.position === 'Defender' && this.spaceshipId) {
+     this.currentShip.cloaked = true
+   } else {
+     return 'had no effect'
+   }
+ }
 }
-
-}());
-
-// it('should charge its phasers when a gunner calls `chargePhasers`', function() {
-//   // console.log(tristian.currentShip)
-//   tristan.chargePhasers()
-//   expect(spaceship.phasersCharge).toBe('uncharged')
-//   katie.chargePhasers()
-//   expect(spaceship.phasersCharge).toBe('charged!')
-// })
-//
-// it('should have its warp drive set to "engaged" only when the pilot uses `engageWarpDrive`', function() {
-//   jon.engageWarpDrive()
-//   expect(spaceship.warpDrive).toBe('disengaged')
-//   tristan.engageWarpDrive()
-//   expect(spaceship.warpDrive).toBe('engaged!')
-// })
-//
-// it('should cloak when a defender `setsInvisibility`', function() {
-//   katie.setsInvisibility()
-//   expect(spaceship.cloaked).toBe(false)
-//   jon.setsInvisibility()
-//   expect(spaceship.cloaked).toBe(true)
-// })
-// })
-// })
--- spaceship.js
+++ spaceship.js
@@ -1,20 +1,13 @@
-
-
-
- const Spaceship = (function createSpaceship(){
-  // var id = 0;
-return class {
-  constructor(data){
-    this.id = data.id
-    this.name = data.name;
-    this.phasers = data.phasers;
-    this.shields = data.shields
-    Store.data.spaceships.push(Object.assign({}, {spaceships: [...Store.data.spaceships, this]}))
-  }
-  crewMembers(){
-      return Store.data.crewMembers.filter((crewMember) => {
-        return crewMember.ship_id === this.id;
-      })}
+class Spaceship {
+ constructor(object) {
+   this.id = object.id;
+   this.name = object.name;
+   this.phasers = object.phasers;
+   this.shields = object.shields;
+   this.phasersCharge = 'uncharged'
+   this.warpDrive = 'disengaged'
+   this.cloaked = false
+   this.docked = true
+   Store.data['spaceships'].push(this)
+ }
 }
-
-}());
